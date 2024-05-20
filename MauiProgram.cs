3b539b34a4b37b03f361819c74bcb7e38a82f9c0
Natthaphon Takhap <Jas39gripen@hotmail.se>
2024-05-20 06:03:49 +0200

    search location

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -6,6 +6,8 @@ using MudBlazor.Services;
 using ShopMate.Data;
 using ShopMate.Service;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 
 namespace ShopMate
 {
@@ -21,6 +23,7 @@ namespace ShopMate
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 }
+                
                 );
 
             builder.Services.AddMauiBlazorWebView();
@@ -28,9 +31,10 @@ namespace ShopMate
             builder.Services.AddSingleton<IRecipe, RecipeRepository>();
             builder.Services.AddSingleton<IShopCart, ShopCartRepository>();
 
-            //builder.Services.AddSingleton<IFileService, FileService>();
             builder.Services.AddMudServices();
-           
+            builder.Services.AddHttpClient();
+
+
             var dbContext = new LocalDbService();
             //dbContext.Database.EnsureDeleted();
             dbContext.Database.EnsureCreated();
@@ -38,7 +42,7 @@ namespace ShopMate
 
 #if DEBUG
             builder.Services.AddBlazorWebViewDeveloperTools();
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,35 @@
-﻿window.addClass = function (className) {
-    document.body.classList.add(className);
-};
+﻿function initializeSwipeEvents() {
+    document.addEventListener('swiped-left', function () {
+        const nextTab = document.querySelector('.mud-tab.mud-tab-active').nextElementSibling;
+        if (nextTab) {
+            nextTab.click();
+        }
+    });
 
-window.removeClass = function (className) {
-    document.body.classList.remove(className);
-};
+    document.addEventListener('swiped-right', function () {
+        const prevTab = document.querySelector('.mud-tab.mud-tab-active').previousElementSibling;
+        if (prevTab) {
+            prevTab.click();
+        }
+    });
+}
+
+function initializeSwipeEvents() {
+    // Initialize swipe events if necessary
+}
+
+function navigateWithAnimation(url) {
+    const pageElement = document.querySelector('.page');
+    pageElement.classList.add('slide-out-right');
+
+    setTimeout(() => {
+        pageElement.classList.remove('slide-out-right');
+        window.Blazor.navigateTo(url);
+        pageElement.classList.add('slide-in-right');
+        setTimeout(() => {
+            pageElement.classList.remove('slide-in-right');
+        }, 500); // Match this duration with the CSS animation duration
+    }, 500); // Match this duration with the CSS animation duration
+}
+
+    
