144e9256316066f7536825d509805bfce44d1305
natthaphon takhap <Jas39gripen@hotmail.se>
2024-05-21 13:26:16 +0200

    drawer wont close

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,35 +1,51 @@
-﻿function initializeSwipeEvents() {
-    document.addEventListener('swiped-left', function () {
-        const nextTab = document.querySelector('.mud-tab.mud-tab-active').nextElementSibling;
-        if (nextTab) {
-            nextTab.click();
-        }
-    });
+﻿//function initializeSwipeEvents() {
+//    document.addEventListener('swiped-left', function () {
+//        const nextTab = document.querySelector('.mud-tab.mud-tab-active').nextElementSibling;
+//        if (nextTab) {
+//            nextTab.click();
+//        }
+//    });
 
-    document.addEventListener('swiped-right', function () {
-        const prevTab = document.querySelector('.mud-tab.mud-tab-active').previousElementSibling;
-        if (prevTab) {
-            prevTab.click();
-        }
-    });
-}
+//    document.addEventListener('swiped-right', function () {
+//        const prevTab = document.querySelector('.mud-tab.mud-tab-active').previousElementSibling;
+//        if (prevTab) {
+//            prevTab.click();
+//        }
+//    });
+//}
 
-function initializeSwipeEvents() {
-    // Initialize swipe events if necessary
-}
+//function initializeSwipeEvents() {
+//    // Initialize swipe events if necessary
+//}
 
-function navigateWithAnimation(url) {
-    const pageElement = document.querySelector('.page');
-    pageElement.classList.add('slide-out-right');
+//function closeDrawers() {
+//    const drawers = document.querySelectorAll('.mud-drawer');
+//    drawers.forEach(drawer => {
+//        drawer.classList.remove('mud-drawer--open');
+//    });
+//}
 
-    setTimeout(() => {
-        pageElement.classList.remove('slide-out-right');
-        window.Blazor.navigateTo(url);
-        pageElement.classList.add('slide-in-right');
-        setTimeout(() => {
-            pageElement.classList.remove('slide-in-right');
-        }, 500); // Match this duration with the CSS animation duration
-    }, 500); // Match this duration with the CSS animation duration
-}
+//function navigateWithAnimation(url) {
+//    closeDrawers();
 
-    
+//    // Wait for a short duration to ensure drawers are closed
+//    setTimeout(() => {
+//        // Perform navigation
+//        window.Blazor.navigateTo(url);
+
+//        // Wait for the navigation to complete and the new page content to be rendered
+//        setTimeout(() => {
+//            const pageElement = document.querySelector('.page');
+
+//            // Remove any previous slide-in class
+//            pageElement.classList.remove('slide-in-right');
+
+//            // Trigger reflow to restart the animation
+//            void pageElement.offsetWidth;
+
+//            // Add the slide-in animation class
+//            pageElement.classList.add('slide-in-right');
+//        }, 50); // Short delay to allow Blazor to update the DOM
+//    }, 200); // Allow some time for drawers to close
+//}
+
